4e9dfbabe6d988ab91bf57f5ed1001e85dcf740e
Zlatan Bekric <zlaja@Zlatans-MacBook-Pro.local>
2023-09-06 17:41:37 +0200

    starter-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-import React, {useState} from 'react';
-import {PasswordBox} from './components/PasswordBox';
-import {PasswordForm} from './components/PasswordForm';
 function App() {
-  const [state, setState] = useState({
-    uppercase: false,
-    lowercase: false,
-    numbers: false,
-    symbols: false,
-    charactersLength: 0,
-    password: '',
-  });
-  return (
-    <main>
-      <h3>Password Generator</h3>
-      <PasswordBox {...state} />
-      <PasswordForm {...state} setState={setState} />
-    </main>
-  );
-}
-
-function SSS() {
-  return (
-    <>
-      <h1>sss</h1>
-      <p>sssss</p>
-    </>
-  );
+  return null;
 }
 
 export default App;
