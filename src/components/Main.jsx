ff085034191866c3b70ee53977f6c8c8247dbb9d
glishh <glisicgrozdana@gmail.com>
2023-09-08 21:26:23 +0200

    update images path

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,18 @@
 import React, {useState} from 'react';
 
-export default function Main() {
-  const [mainDetails, setMainDetails] = useState({
-    name: 'Mercury',
-    overview: {
-      content:
-        "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
-      source: 'https://en.wikipedia.org/wiki/Mercury_(planet)',
-    },
-    structure: {
-      content: '',
-      source: '',
-    },
-    geology: {
-      content: '',
-      source: '',
-    },
-    images: {
-      planet: './assets/planet-mercury.svg',
-      internal: '',
-      geology: '',
-    },
-  });
+export default function Main({mainDetails = {}}) {
   return (
     <main>
-      <img className="planet-img" src="./assets\images/planet-mercury.svg" alt="" />
+      <img className="planet-img" src={mainDetails?.images?.planet} alt="" />
       <div className="main-text">
         <div className="description">
           <h1>{mainDetails.name}</h1>
           <p>{mainDetails?.overview?.content}</p>
           <div className="source">
             <p>Source:</p>
-            <a href={mainDetails?.overview?.source}>
+            <a href={mainDetails?.overview?.source} target="_blank">
               Wikipedia
-              <img src={mainDetails?.images?.planet} alt="" />
+              <img src="./assets/source-icon.svg" alt="" />
             </a>
           </div>
         </div>
