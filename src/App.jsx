afc5a12493ddd0cfb3b08049f4da9e3e7cd7ac0a
MZoranovic <117400219+MZoranovic@users.noreply.github.com>
2023-09-08 21:15:39 +0200

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Footer from './component/Footer';
 import {useState} from 'react';
 import Header from './components/Header';
+import {getPlanetData} from './api';
 
 function App() {
   const [currentPlanet, setCurrentPlanet] = useState('Mercury');
+  const [currentPlanetData, setCurrentPlanetData] = useState({});
   const navigate = (planet) => {
     setCurrentPlanet(planet);
   };
+
+  const getData = async () => {
+    const result = await getPlanetData(currentPlanet);
+    setCurrentPlanetData(result);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [currentPlanet]);
+
   return (
     <>
       <Header navigate={navigate} currentPlanet={currentPlanet} />
-      <Footer />
+      <Footer footerDetails={currentPlanetData} />
     </>
   );
 }
